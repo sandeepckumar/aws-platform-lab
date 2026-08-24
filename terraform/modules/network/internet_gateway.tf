470c9b86ef9861f00199fb4b56f675c42befdfe4
Sandeep Chenna Kumar <sandeep.chenna@ibm.com>
2026-08-24 21:02:57 +0530

    add destroy workflow

--- terraform/modules/network/internet_gateway.tf
+++ terraform/modules/network/internet_gateway.tf
@@ -6,7 +6,7 @@ resource "aws_internet_gateway" "this" {
   }
 }
 
-resource "aws_internet_gateway_attachment" {
+resource "aws_internet_gateway_attachment" "this" {
   count              = var.create_igw ? 1 : 0
   internet_gatway_id = aws_internet_gateway.this.id
   vpc_id             = aws_vpc.this.id
