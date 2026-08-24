2712616a80b10f025c78fb7bc4a95afd2c9b1ad7
Sandeep Chenna Kumar <sandeep.chenna@ibm.com>
2026-08-24 21:18:01 +0530

    remove gateway attachment

--- terraform/modules/network/internet_gateway.tf
+++ terraform/modules/network/internet_gateway.tf
@@ -5,9 +5,3 @@ resource "aws_internet_gateway" "this" {
     Name = var.igw_name
   }
 }
-
-resource "aws_internet_gateway_attachment" "this" {
-  count              = var.create_igw ? 1 : 0
-  internet_gateway_id = aws_internet_gateway.this[0].id
-  vpc_id             = aws_vpc.this.id
-}
